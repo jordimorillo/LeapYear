e460fccd6f3fdc6056bb93a359d237c49e5bfb32
Jordi <jmdesarrollo82@gmail.com>
2020-06-30 20:07:43 +0200

    Add a test to instantiate a year

---
+++ tests/Domain/Year.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace Src\Domain;
+
+
+class Year
+{
+
+    /**
+     * @var int
+     */
+    private $year;
+
+    /**
+     * Year constructor.
+     * @param int $year
+     */
+    public function __construct(int $year) {
+        $this->year = $year;
+    }
+
+    /**
+     * @return int
+     */
+    public function getYear(): int
+    {
+        return $this->year;
+    }
+
+}
--- tests/Domain/YearTest.php
+++ tests/Domain/YearTest.php
@@ -5,9 +5,14 @@ namespace Tests;
 
 
 use PHPUnit\Framework\TestCase;
+use Src\Domain\Year;
 
 
 class YearTest extends TestCase
 {
-
+    public function testThatICanInstantiateAYear(): void
+    {
+        $year = new Year(random_int(0, 9999));
+        $this->assertIsInt($year->getYear());
+    }
 }
